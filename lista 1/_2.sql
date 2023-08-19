ab90b7e67630b2fd590d22bca895ef260f891671
nicolasceccato <nicolasceccato@hotmail.com>
2023-08-19 16:05:30 -0300

    exercicio 2

---
+++ lista 1/_2.sql
@@ -0,0 +1,3 @@
+-- Forneça uma expressão SQL para buscar a quantidade total de especialidades veterinárias distintas encontradas na tabela funcionários. 
+
+SELECT COUNT(distinct especialidade) from funcionarios;
