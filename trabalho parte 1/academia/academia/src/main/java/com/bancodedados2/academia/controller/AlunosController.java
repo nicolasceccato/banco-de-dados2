5f4034202aa812b6eae773f5719af02cf1b1c7fe
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2023-09-04 16:55:32 -0300

    criando AlunosRepository

--- trabalho parte 1/academia/academia/src/main/java/com/bancodedados2/academia/controller/AlunosController.java
+++ trabalho parte 1/academia/academia/src/main/java/com/bancodedados2/academia/controller/AlunosController.java
@@ -1,6 +1,8 @@
 package com.bancodedados2.academia.controller;
 
 import com.bancodedados2.academia.entities.Alunos;
+import com.bancodedados2.academia.repository.AlunosRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,8 +11,11 @@ import java.util.List;
 @RestController
 public class AlunosController {
 
+    @Autowired
+    private AlunosRepository alunosRepository;
+
     @GetMapping("/alunos")
-    public String listAllAlunos() {
-        return ("alunos");
+    public List<Alunos> listAllAlunos() {
+        return alunosRepository.findAll();
     }
 }
