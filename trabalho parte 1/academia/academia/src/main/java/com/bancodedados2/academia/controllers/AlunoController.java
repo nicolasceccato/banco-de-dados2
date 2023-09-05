a1a195e2d0d831960ccacc0615e250d3fce988ae
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2023-09-05 08:32:59 -0300

    ajustando o controller

--- trabalho parte 1/academia/academia/src/main/java/com/bancodedados2/academia/controllers/AlunoController.java
+++ trabalho parte 1/academia/academia/src/main/java/com/bancodedados2/academia/controllers/AlunoController.java
@@ -2,6 +2,7 @@ package com.bancodedados2.academia.controllers;
 
 import com.bancodedados2.academia.entities.Aluno;
 import com.bancodedados2.academia.services.AlunoService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin("*")
 @RequestMapping("/alunos")
 public class AlunoController {
 
@@ -29,20 +31,19 @@ public class AlunoController {
     }
 
     @PostMapping
-    public Aluno createAluno(@RequestBody Aluno aluno) {
-        return alunoService.save(aluno);
+    public ResponseEntity<Aluno> createAluno(@Valid @RequestBody Aluno aluno) {
+        return ResponseEntity.status(201).body(alunoService.save(aluno));
     }
 
     @PutMapping
-    public Aluno editAluno(@RequestBody Aluno aluno) {
-        return alunoService.save(aluno);
+    public ResponseEntity<Aluno> editAluno(@RequestBody Aluno aluno) {
+        return ResponseEntity.status(200).body(alunoService.save(aluno));
     }
 
-//    @DeleteMapping("/{cpf}")
-//    public Optional<Aluno> deleteAluno(@PathVariable String cpf) {
-//        Optional<Aluno> aluno = alunoService.findById(cpf);
-//        alunoService.deleteById(cpf);
-//        return aluno;
-//    }
+    @DeleteMapping("/{cpf}")
+    public ResponseEntity<Aluno> deleteAluno(@PathVariable String cpf) {
+        alunoService.deleteById(cpf);
+        return ResponseEntity.status(204).build();
+    }
 
 }
